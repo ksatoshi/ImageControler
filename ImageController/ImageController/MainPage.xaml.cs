f2172129b94ddd9bd477da1e0e3899bd4da21840
ksatoshi <ksatoshi@mail.ksatoshi.dev>
2023-09-20 19:34:52 +0900

    カラーテーマをライトモードに固定する処理を追加

--- ImageController/ImageController/MainPage.xaml.cs
+++ ImageController/ImageController/MainPage.xaml.cs
@@ -1,120 +1,98 @@
-﻿using System.Collections;
-using System.Net.Mime;
-using Microsoft.Maui.Platform;
+﻿namespace ImageController;
 
-namespace ImageController
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    public MainPage()
     {
+        InitializeComponent();
+        MainPageLayout();
+        Application.Current.UserAppTheme = AppTheme.Light; // ライトモードに固定
+    }
 
-        public MainPage()
+    private void MainPageLayout()
+    {
+        Image nonLoadingPlaceHolder;
+
+        try
         {
-            InitializeComponent();
-            this.MainPageLayout();
+            //画像未読み込み時用のプレースホルダーをロード
+            nonLoadingPlaceHolder = new Image { Source = ImageSource.FromFile("no_image.png"), Aspect = Aspect.Center };
         }
-        
-        private void MainPageLayout()
+        catch (Exception e)
         {
-            Image nonLoadingPlaceHolder;
-            
-            try
-            {
-                //画像未読み込み時用のプレースホルダーをロード
-                nonLoadingPlaceHolder = new Image { Source = ImageSource.FromFile("no_image.png"),Aspect = Aspect.Center};
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-
-            
-            /*
-             * ImageControllerResultCheckerと同様に
-             * Row*4,
-             * Column*2
-             * を定義
-             */
-            var rootGrid = new Grid
-            {
-                RowDefinitions =
-                {
-                    new RowDefinition{Height = GridLength.Auto},
-                    new RowDefinition(),
-                },
-                ColumnDefinitions =
-                {
-                    /*
-                     * 第一列:画面の7割
-                     * 第二列:画面の3割
-                     */
-                    new ColumnDefinition{Width = new GridLength(7,GridUnitType.Star)},
-                    new ColumnDefinition{Width = new GridLength(3,GridUnitType.Star)}
-                }
-            };
-            
-            //テスト用のUIパーツ
-            /*var dualRowBoxView = new BoxView { Color = Colors.Blue };
-            Grid.SetRow(dualRowBoxView, 0);
-            Grid.SetColumnSpan(dualRowBoxView,2);
-            
-            rootGrid.Add(dualRowBoxView);*/
-
-            /*var menuBar = new MenuBar();
-            menuBar.Add(new MenuBarItem{Text = "ファイル"});
-            menuBar.Add(new MenuBarItem{Text = "ヒストリー"});
-            menuBar.Add(new MenuBarItem{Text = "ヘルプ"});
-            
-            Grid.SetRow(menuBar,0);
-            Grid.SetColumnSpan(menuBar,2);*/
-
-            
-            // コントロール用のボタン群を生成
-            var buttonArrayStack = new StackLayout
-            {
-                Margin = new Thickness(20), Orientation = StackOrientation.Horizontal,
-                HorizontalOptions = LayoutOptions.Start,
-                Spacing = 6
-            };
-            
-            buttonArrayStack.Add(new Button{Text = "読み込み"});
-            buttonArrayStack.Add(new Button{Text = "保存"});
-            buttonArrayStack.Add(new Button{Text = "比較"});
-            buttonArrayStack.Add(new Button{Text = "削除"});
-            
-            // 各種パラメーター入力用テキストボックス群
-            var filterSettingsStack = new StackLayout
-            {
-                Margin = new Thickness(20),
-                Spacing = 6
-            };
-
-            var filterList = new List<string>();
-            filterList.Add("ガウシアンフィルタ");
-            filterList.Add("メディアンフィルタ");
-            filterList.Add("グレースケール");
-
-            var filterPicker = new Picker { Title = "フィルターセレクター" };
-            filterPicker.ItemsSource = filterList;
-            
-            filterSettingsStack.Add(filterPicker);
-            filterSettingsStack.Add(new Label{Text = "kernel_size"});
-            filterSettingsStack.Add(new Entry{Placeholder = "kernel_size"});
-            filterSettingsStack.Add(new Label{Text = "閾値1"});
-            filterSettingsStack.Add(new Entry{Placeholder = "閾値1"});
-            filterSettingsStack.Add(new Label{Text = "閾値2"});
-            filterSettingsStack.Add(new Entry{Placeholder = "閾値2"});
-            filterSettingsStack.Add(new Button{Text = "実行"});
-            
-            rootGrid.Add(buttonArrayStack,1,0);
-            rootGrid.Add(filterSettingsStack,1,1);
-            
-            Grid.SetColumn(nonLoadingPlaceHolder,0);
-            Grid.SetRowSpan(nonLoadingPlaceHolder,2);
-            rootGrid.Add(nonLoadingPlaceHolder,0,0);
-            
-            Content = rootGrid;
+            Console.WriteLine(e);
+            throw;
         }
-        
+
+
+        /*
+         * ImageControllerResultCheckerと同様に
+         * Row*2,
+         * Column*2
+         * を定義
+         */
+        var rootGrid = new Grid
+        {
+            RowDefinitions =
+            {
+                new RowDefinition { Height = GridLength.Auto },
+                new RowDefinition()
+            },
+            ColumnDefinitions =
+            {
+                /*
+                 * 第一列:画面の7割
+                 * 第二列:画面の3割
+                 */
+                new ColumnDefinition { Width = new GridLength(7, GridUnitType.Star) },
+                new ColumnDefinition { Width = new GridLength(3, GridUnitType.Star) }
+            }
+        };
+
+        // コントロール用のボタン群を生成
+        var buttonArrayStack = new StackLayout
+        {
+            Margin = new Thickness(20), Orientation = StackOrientation.Horizontal,
+            HorizontalOptions = LayoutOptions.Start,
+            Spacing = 6
+        };
+
+        buttonArrayStack.Add(new Button { Text = "読み込み"});
+        buttonArrayStack.Add(new Button { Text = "保存" });
+        buttonArrayStack.Add(new Button { Text = "比較" });
+        buttonArrayStack.Add(new Button { Text = "削除" });
+
+        // 各種パラメーター入力用テキストボックス群
+        var filterSettingsStack = new StackLayout
+        {
+            Margin = new Thickness(20),
+            Spacing = 6
+        };
+
+        var filterList = new List<string>();
+        filterList.Add("ガウシアンフィルタ");
+        filterList.Add("メディアンフィルタ");
+        filterList.Add("グレースケール");
+
+        var filterPicker = new Picker { Title = "フィルターセレクター" };
+        filterPicker.ItemsSource = filterList;
+
+        filterSettingsStack.Add(filterPicker);
+        filterSettingsStack.Add(new Label { Text = "kernel_size" });
+        filterSettingsStack.Add(new Entry { Placeholder = "kernel_size" });
+        filterSettingsStack.Add(new Label { Text = "閾値1" });
+        filterSettingsStack.Add(new Entry { Placeholder = "閾値1" });
+        filterSettingsStack.Add(new Label { Text = "閾値2" });
+        filterSettingsStack.Add(new Entry { Placeholder = "閾値2" });
+        filterSettingsStack.Add(new Button { Text = "実行" });
+
+        rootGrid.Add(buttonArrayStack, 1); // column:1,row:0
+        rootGrid.Add(filterSettingsStack, 1, 1); // column:1,row:1
+
+        Grid.SetColumn(nonLoadingPlaceHolder, 0);
+        Grid.SetRowSpan(nonLoadingPlaceHolder, 2);
+        rootGrid.Add(nonLoadingPlaceHolder, 0); // column:0,row:0
+
+        Content = rootGrid;
     }
 }
