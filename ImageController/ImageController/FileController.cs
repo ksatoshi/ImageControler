2294cc1fa8500ced2c7b1dd0bca8e21d0b448fe8
ksatoshi <ksatoshi@mail.ksatoshi.dev>
2023-09-21 20:36:31 +0900

    FiliePickerを開いて画像のパスを得る関数を作成

---
+++ ImageController/ImageController/FileController.cs
@@ -0,0 +1,28 @@
+﻿using System.Formats.Asn1;
+
+namespace ImageController;
+
+
+public class FileController
+{
+    protected PickOptions options;
+
+
+    public async Task<FileResult> FileSelect()
+    {
+        try
+        {
+            var result = await FilePicker.Default.PickAsync(options);
+            if (result != null)
+            {
+                return result;
+            }
+        }
+        catch (Exception ex)
+        {
+            // TODO:なんらかの例外処理
+        }
+
+        return null;
+    }
+}
---
+++ ImageController/ImageController/ImageFileController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageController
+{
+    public class ImageFileController : FileController
+    {
+        public ImageFileController()
+        {
+            var customFileType = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+            {
+                {DevicePlatform.WinUI, new[] {"bmp","dib","jpeg","jpg","jpe","png","pbm","pgm","ppm","sr","ras","tiff","tif"}},
+                //TODO: macOS対応のUUtypeによる記述を追加
+            });
+
+            options = new()
+            {
+                PickerTitle = "画像ファイルを選択してください",
+                FileTypes = customFileType
+            };
+        }
+    }
+}
