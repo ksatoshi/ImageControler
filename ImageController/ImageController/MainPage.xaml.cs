bd21301810872e8258aad60b3e44c4e06a4a2585
ksatoshi <kswhale@sirius.ocn.ne.jp>
2023-09-22 20:29:34 +0900

    選択した画像を表示するように変更

--- ImageController/ImageController/MainPage.xaml.cs
+++ ImageController/ImageController/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace ImageController;
+﻿using OpenCvSharp;
+using System.Diagnostics;
+
+namespace ImageController;
 
 public partial class MainPage : ContentPage
 {
@@ -11,5 +14,18 @@ public partial class MainPage : ContentPage
         imageFileController = new ImageFileController();
     }
 
-   
+    private async void Load_Button_Clicked(object sender, EventArgs e)
+    {
+        // ファイルパスを取得
+        var path =await imageFileController.FileSelect();
+        Debug.WriteLine("path:" +path.FullPath);
+
+        // 画像を読み込み一時ファイルディレクトリに書き出す
+        Mat image = Cv2.ImRead(path.FullPath);
+        imageFileController.ImageFileWriteTmp(image, "tmp.png");
+
+        // 一時ファイルディレクトリからロードし表示する
+        var tmpImagePath = imageFileController.GetTmpFilePath("tmp.png");
+        image_view.Source = tmpImagePath;
+    }
 }
