a08395db912279846485e90938382623dcbc0aa6
ksatoshi <kswhale@sirius.ocn.ne.jp>
2023-09-22 20:29:16 +0900

    ImageFIleControllerに一時ファイルディレクトリに画像を書き出す関数を追加

--- ImageController/ImageController/ImageFileController.cs
+++ ImageController/ImageController/ImageFileController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using OpenCvSharp;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -14,7 +16,7 @@ namespace ImageController
             var customFileType = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
             {
                 {DevicePlatform.WinUI, new[] {"bmp","dib","jpeg","jpg","jpe","png","pbm","pgm","ppm","sr","ras","tiff","tif"}},
-                //TODO: macOS対応のUUtypeによる記述を追加
+                {DevicePlatform.macOS, new[]{"png","jpeg","tiff","bmp"} }
             });
 
             options = new()
@@ -22,6 +24,19 @@ namespace ImageController
                 PickerTitle = "画像ファイルを選択してください",
                 FileTypes = customFileType
             };
+        }
+
+        // 画像を一時ファイルディレクトリに書き出す関数
+        public void ImageFileWriteTmp(Mat image,String fileName)
+        {
+            var cacheDir = FileSystem.Current.CacheDirectory;
+            try
+            {
+                Cv2.ImWrite(cacheDir + fileName, image);
+                Debug.WriteLine("Write:" + cacheDir + "." + fileName);
+            }catch(Exception ex) {
+                Debug.WriteLine(ex.Message);
+            }    
         }
     }
 }
